199317ed9da89762519cfc1288573f3455a9324a
sudharsan94 <sudharsun@@unifisoftware.com>
2019-05-03 22:23:16 +0530

    Change javascript target version from es6 to es5.

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,9 @@
 "use strict";
-const fs = require("fs");
-let html2canvasPath = require.resolve("html2canvas").replace("/npm/index.js", "/html2canvas.min.js");
-class ProtractorScreenShotUtils {
-    constructor(config) {
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var html2canvasPath = require.resolve("html2canvas").replace("/npm/index.js", "/html2canvas.min.js");
+var ProtractorScreenShotUtils = (function () {
+    function ProtractorScreenShotUtils(config) {
         this.isDefaultScreenshotMethod = false;
         if (!config || !config.browserInstance) {
             throw new Error("browser object is required");
@@ -12,35 +13,10 @@ class ProtractorScreenShotUtils {
             this.makeAsDefault();
         }
     }
-    takeScreenshot(options = {}) {
-        let currentContext = this.browser ? this.browser : this, element = options.element ? options.element : currentContext.$("body"), dimensions = options.dimensions || {}, outputPath = options.saveTo || null, html2canvasScript = fs.readFileSync(html2canvasPath, 'utf8'), injectionScript = `var callBack = arguments[arguments.length -1];
-                var dimensions = arguments[1];
-                html2canvas(arguments[0]).then(function(canvas){
-                     
-                     if(Object.keys(dimensions).length == 4) {
-                        console.log("Success");
-                        var croppedCanvas = document.createElement("canvas");
-                        var context = canvas.getContext('2d');
-                        var croppedImage = context.getImageData(dimensions.x,dimensions.y,canvas.width,canvas.height);
-                        console.log(croppedImage)
-                        console.log(canvas.width,canvas.height);
-                        var croppedContext = croppedCanvas.getContext('2d');
-                        croppedCanvas.width = dimensions.width;
-                        croppedCanvas.height = dimensions.height;
-                        croppedContext.putImageData(croppedImage,0,0);
-                        callBack(croppedCanvas.toDataURL())
-                     } else {
-                        callBack(canvas.toDataURL())
-                     }
-              
-                 })`;
-        return currentContext.executeScript(`var scriptEle = document.createElement("script");
-                scriptEle.type = "text/javascript";
-                scriptEle.innerText = function injectScript() { ${html2canvasScript} };
-                document.body.appendChild(scriptEle);
-                document.body.appendChild(scriptEle);
-                injectScript();
-                `).then(function () {
+    ProtractorScreenShotUtils.prototype.takeScreenshot = function (options) {
+        if (options === void 0) { options = {}; }
+        var currentContext = this.browser ? this.browser : this, element = options.element ? options.element : currentContext.$("body"), dimensions = options.dimensions || {}, outputPath = options.saveTo || null, html2canvasScript = fs.readFileSync(html2canvasPath, 'utf8'), injectionScript = "var callBack = arguments[arguments.length -1];\n                var dimensions = arguments[1];\n                html2canvas(arguments[0]).then(function(canvas){\n                     \n                     if(Object.keys(dimensions).length == 4) {\n                        console.log(\"Success\");\n                        var croppedCanvas = document.createElement(\"canvas\");\n                        var context = canvas.getContext('2d');\n                        var croppedImage = context.getImageData(dimensions.x,dimensions.y,canvas.width,canvas.height);\n                        console.log(croppedImage)\n                        console.log(canvas.width,canvas.height);\n                        var croppedContext = croppedCanvas.getContext('2d');\n                        croppedCanvas.width = dimensions.width;\n                        croppedCanvas.height = dimensions.height;\n                        croppedContext.putImageData(croppedImage,0,0);\n                        callBack(croppedCanvas.toDataURL())\n                     } else {\n                        callBack(canvas.toDataURL())\n                     }\n              \n                 })";
+        return currentContext.executeScript("var scriptEle = document.createElement(\"script\");\n                scriptEle.type = \"text/javascript\";\n                scriptEle.innerText = function injectScript() { " + html2canvasScript + " };\n                document.body.appendChild(scriptEle);\n                document.body.appendChild(scriptEle);\n                injectScript();\n                ").then(function () {
             return currentContext.executeAsyncScript(injectionScript, element.getWebElement(), dimensions).then(function (base64String) {
                 base64String = base64String.replace(/^data:image\/png;base64,/, "");
                 if (outputPath) {
@@ -49,19 +25,20 @@ class ProtractorScreenShotUtils {
                 return base64String;
             });
         });
-    }
-    makeAsDefault() {
+    };
+    ProtractorScreenShotUtils.prototype.makeAsDefault = function () {
         if (!this.isDefaultScreenshotMethod) {
             this.originalScreenshotMethod = this.browser.takeScreenshot;
             this.browser.takeScreenshot = this.takeScreenshot.bind(this.browser);
             this.isDefaultScreenshotMethod = true;
         }
-    }
-    resetToDefault() {
+    };
+    ProtractorScreenShotUtils.prototype.resetToDefault = function () {
         if (this.originalScreenshotMethod && this.isDefaultScreenshotMethod) {
             this.browser.takeScreenshot = this.originalScreenshotMethod.bind(this.browser);
             this.isDefaultScreenshotMethod = false;
         }
-    }
-}
+    };
+    return ProtractorScreenShotUtils;
+}());
 exports.ProtractorScreenShotUtils = ProtractorScreenShotUtils;
